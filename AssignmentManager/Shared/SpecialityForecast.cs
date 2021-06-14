dc4892c8c4538dd018c3db061491d50ae79b099b
Erartria <ujkwf0at4vgb@mail.ru>
2021-06-14 21:08:59 +0300

    feat: frontend implementation starts

---
+++ AssignmentManager/Shared/SpecialityForecast.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AssignmentManager.Shared
+{
+    public class SpecialityForecast
+    {
+        public int Id { get; set; }
+        public string StudyTypeName { get; set; }
+        
+        public string Code { get; set; }
+            
+    }
+}
