ad259eaf488b8e1c5e46658acb57847090c8175b
Erartria <ujkwf0at4vgb@mail.ru>
2021-06-19 19:11:45 +0300

    documentation: integrate Swagger into project

--- AssignmentManager/Server/Startup.cs
+++ AssignmentManager/Server/Startup.cs
@@ -1,4 +1,6 @@
 using System;
+using System.IO;
+using System.Reflection;
 using AssignmentManager.Server.Models;
 using AssignmentManager.Server.Persistence.Contexts;
 using AssignmentManager.Server.Services;
@@ -9,6 +11,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace AssignmentManager.Server
 {
@@ -50,7 +53,11 @@ namespace AssignmentManager.Server
             services.AddControllersWithViews();
             services.AddRazorPages();
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-            
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "AssigmentManager API", Version = "v1" });
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -58,6 +65,11 @@ namespace AssignmentManager.Server
             IWebHostEnvironment env,
             AppDbContext context)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+            });
             //context.Database.Migrate();
             if (env.IsDevelopment())
             {
@@ -88,6 +100,8 @@ namespace AssignmentManager.Server
                 endpoints.MapControllers();
                 endpoints.MapFallbackToFile("index.html");
             });
+
+            
         }
     }
 }
