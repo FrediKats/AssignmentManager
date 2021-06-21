472bc903d3bf25edd84041cc311fa688e015919d
Erartria <ujkwf0at4vgb@mail.ru>
2021-06-21 11:56:32 +0300

    merge: fix merge

--- AssignmentManager/Server/Resources/GroupResourceBriefly.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AssignmentManager.Server.Resources
-{
-    public class GroupResourceBriefly
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public int? SpecialityId { get; set; }
-    }
-}
